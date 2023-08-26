ce08ab7f9909e55efda89e9a791d3cdfa885e49f
Toshiyuki HIRAKI <130117931+hirakinii@users.noreply.github.com>
2023-08-26 15:23:55 +0900

    Update module_info.py

--- nii_dg/module_info.py
+++ nii_dg/module_info.py
@@ -6,11 +6,13 @@ Configuration parameters for the nii_dg package.
 """
 
 GH_OWNER: str = "hirakinii"
+"""str: The GitHub repository owner for the nii_dg package."""
 
 GH_REPO_NAME: str = "nii-dg"
+"""str: The GitHub repository name for the nii_dg package."""
 
 GH_REPO: str = f"{GH_OWNER}/{GH_REPO_NAME}"
-"""str: The GitHub repository name for the nii_dg package."""
+"""str: The GitHub repository path for the nii_dg package."""
 
 GH_REF: str = "demo-myschema-01"
 """str: The GitHub reference (tag or branch) for the nii_dg package."""
